7d442f7cfcdaad45d107a2211d86836063549852
coque <coquelet.samuel@gmail.com>
2021-11-30 11:49:35 +0100

    fini

--- app.js
+++ app.js
@@ -31,10 +31,19 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [citroen, peugeot, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
 
+for (let x = 0; x < voitures.collection.length; x++) {
+    let para = document.getElementById("para");
+    let spanP = document.getElementById('sp');
+    spanP.innerHTML = voitures.collection[x].color + ", ";
+    voitures.collection[x].ouvrirPorte();
+
+    para.append(spanP);
+
+}
 
